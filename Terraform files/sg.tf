4ceca9ce4a17572b68271bcd9d96f6b19f69d996
12Tubo <94327404+12Tubo@users.noreply.github.com>
2022-07-02 12:36:11 -0400

    Create sg.tf

---
+++ Terraform files/sg.tf
@@ -0,0 +1,40 @@
+
+# creating security group
+Resources “aws_security_group” “demosg” {
+  Vpc_id =  “ ${ aws_vpc.main.id } ”
+  #inbound rules
+  # http access from anywhere
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [ “0.0.0.0/0”]
+
+  # https access from anywhere
+  ingress {
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [ “0.0.0.0/0”]
+
+  # ssh access from anywhere
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [ “0.0.0.0/0”]
+
+  # Outbound Rules
+  # Internet access to anywhere
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+Tags = {
+  Name = “web sg”
+}
+}
+
+
