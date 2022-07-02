2c3015d5d5f6de64efa2070a8baf63dca6c881f5
12Tubo <94327404+12Tubo@users.noreply.github.com>
2022-07-02 11:56:04 -0400

    Create subnet.tf

---
+++ Terraform files/subnet.tf
@@ -0,0 +1,67 @@
+# Creating Ist web subnet
+resource "aws_subnet" "public-subnet-1" {
+  vpc_id     = " ${aws_vpc.main.id} ”
+  cidr_block =  “${var.subnet_cidr}”
+  map_public_ip_on_launch = true
+  availability_zone = “us-east-1a”
+
+  tags = {
+    Name = "web subnet 1"
+  }
+}
+
+# creating 2nd web subnet
+resource "aws_subnet" "public-subnet-2" {
+  vpc_id     = “${aws_vpc.main.id}”
+  cidr_block = “ $ {var.subnet1_cidr} ”
+  map_public_ip_on_launch = true
+  availability_zone = “us-east-1b”
+
+  tags = {
+    Name = "web subnet 2"
+  }
+}
+
+
+# creating ist application subnet
+resource "aws_subnet" "application-subnet-1" {
+  vpc_id     = “${aws_vpc.main.id}”
+  cidr_block = “${var.subnet2_cidr}”
+  map_public_ip_on_launch = false
+  availability_zone = “us-east-1b”
+
+  tags = {
+    Name = "application subnet 1"
+
+
+# creating 2nd application subnet
+resource "aws_subnet" "application-subnet-2" {
+  vpc_id     = “${aws_vpc.main.id}”
+  cidr_block =  “${var.subnet3_cidr}”
+  map_public_ip_on_launch = false
+  availability_zone = “us-east-1b”
+
+  tags = {
+    Name = "application subnet 1"
+
+
+# creating ist database subnet
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id     = “${aws_vpc.main.id}”
+  cidr_block =   “${var.subnet4_cidr}”
+  map_public_ip_on_launch = false
+  availability_zone = “us-east-1a”
+
+  tags = {
+    Name = "database subnet 1"
+
+
+# creating 2nd database subnet
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id     = “${aws_vpc.main.id}”
+  cidr_block =   "${var.subnet5_cidr}"
+  map_public_ip_on_launch = false
+  availability_zone = “us-east-1a”
+
+  tags = {
+    Name = "database subnet 2"
