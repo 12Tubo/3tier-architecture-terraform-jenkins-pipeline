adda36e93fccfe8cda2c2ccf8a4c1669fbb79972
12Tubo <omofoglory@yahoo.com>
2022-07-03 23:28:36 -0400

    "commit to main"

--- Terraform files/ec2.tf
+++ Terraform files/ec2.tf
@@ -1,42 +1,42 @@
 # Creating its ec2 instance in public subnet
- resource "aws instance" "demo instance" {
-
- 
-   ami         = "ami-0cff7528ff583bf9a"
-   instance_type = "t2.micro"
-   count = 1
-   key_name = "master"
-   vpc_security_group_ids =   "${aws_security_group. Sg.id}"
-   subnet_id = "${aws_subnet.public_subnet1.id}"
-   associate_public_ip_address = true
-   
-   user_data = "${ file("data.sh") }"
+resource "aws instance" "demo instance" {
 
 
- tags = {
+  ami                         = "ami-0cff7528ff583bf9a"
+  instance_type               = "t2.micro"
+  count                       = 1
+  key_name                    = "master"
+  vpc_security_group_ids      = aws_security_group.Sg.id
+  subnet_id                   = aws_subnet.public_subnet1.id
+  associate_public_ip_address = true
+
+  user_data = file("data.sh")
+
+
+  tags = {
     Name = "public instance 1"
-}
+  }
 }
 
-   
-   # creating 2nd ec2 instance in public subnet
- 
- resource "aws instance" "demo instance" {
+
+# creating 2nd ec2 instance in public subnet
+
+resource "aws instance" "demo instance" {
 
 
-   ami         = ami-0cff7528ff583bf9a
-   instance_type = t2.micro
-   count = 1
-   key_name = master
-   
-   vpc_security_group_ids =   "${ aws_security_group. Sg.id}" 
-   
-   subnet_id =  "${ aws_subnet.public_subnet1.id}"
-   
-   associate_public_ip_address = true
-   
-   user_data = "${file("data.sh") }"
- tags = {
-   Name = "public instance 2"
-}
+  ami           = ami-0cff7528ff583bf9a
+  instance_type = t2.micro
+  count         = 1
+  key_name      = master
+
+  vpc_security_group_ids = aws_security_group.Sg.id
+
+  subnet_id = aws_subnet.public_subnet1.id
+
+  associate_public_ip_address = true
+
+  user_data = file("data.sh")
+  tags = {
+    Name = "public instance 2"
+  }
 }
