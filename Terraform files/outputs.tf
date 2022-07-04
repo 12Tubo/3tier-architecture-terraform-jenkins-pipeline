f9e39f73e7d569bd6d63583d410d0f604370c6b4
12Tubo <omofoglory@yahoo.com>
2022-07-04 06:59:25 -0400

    "commit to main"

--- Terraform files/outputs.tf
+++ Terraform files/outputs.tf
@@ -1,5 +1,5 @@
 #getting the DNS of load balancer
  output "lb_dns_name" {
-  Description = "The DNS name of the load balancer"
+  description = "The DNS name of the load balancer"
   value       = aws_lb.external-alb.dns_name
 }
